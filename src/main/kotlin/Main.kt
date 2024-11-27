770c259d8f2c83187bf6df68aa0f86132a2380e2
D1MELLO <sergio_dimello@outlook.com>
2024-11-27 10:03:52 -0300

    First Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+package com.alura.dimello
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+    println(json)
+
+    val meuJogo = Jogo()
+    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    meuJogo.capa = "https:\\/\\/shared.fastly.steamstatic.com\\/store_item_assets\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1702934705"
+//    meuJogo.descricao = "Jogo do Batima"
+
+    println(meuJogo)
+}
